c122e8943665d0eadfadf38de8c4afb165e82342
Will Goad <willgoad@btinternet.com>
2023-08-25 20:51:54 +0100

    Menu

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,7 @@ export default function Home() {
   const [action2Text, setAction2Text] = React.useState('View Examples   →');
   const [exampleMode, setExampleMode] = React.useState(false);
   const [exampleSite, setExampleSite] = React.useState(webFrameworks[0]);
+  const [menuOpen, setMenuOpen] = React.useState(false);
 
   const onExampleModeOn = () => {
     if (exampleMode === false) {
@@ -25,12 +26,16 @@ export default function Home() {
     }
   }
 
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  }
+
 
   return (
     <main className="min-h-screen  p-12">
       <div className="flex flex-row justify-center gap-16">
         <div className={!exampleMode ? "flex flex-col items-center justify-around" : "flex flex-col items-center justify-around max-w-xl"}>
-          <header className="flex items-center justify-between w-full max-w-5xl">
+          <header className="flex items-center sm:justify-between justify-center w-full max-w-5xl">
             <a href="https://portco.de">
               <Image
                 src="/portcodelogo.png"
@@ -39,7 +44,17 @@ export default function Home() {
                 height={100}
               />
             </a>
-            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" /></svg>
+            {menuOpen && <div className="hidden lg:flex flex-row items-center justify-center gap-5 transition-all">
+              <p>Signup</p>
+              <p>•</p>
+              <p>Login</p>
+              <p>•</p>
+              <p>Stats for Hackers</p>
+              <p>•</p>
+              <p>Credits</p>
+            </div>}
+            {!menuOpen && <svg xmlns="http://www.w3.org/2000/svg" className="hidden lg:block" height="1em" viewBox="0 0 448 512" onClick={toggleMenu}><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" /></svg>}
+            {menuOpen && <svg xmlns="http://www.w3.org/2000/svg" className="hidden lg:block" height="1em" viewBox="0 0 384 512" onClick={toggleMenu}><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" /></svg>}
           </header>
 
 
@@ -47,7 +62,7 @@ export default function Home() {
             <div className='flex flex-col'>
               <div className="grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
 
-                <div className="flex flex-col justify-center w-full mb-8 lg:mb-0 lg:col-span-2">
+                <div className={exampleMode ? "hidden sm:flex flex-col items-center justify-center w-full mb-8 lg:mb-0 lg:col-span-2" : "flex flex-col items-center justify-center w-full mb-8 lg:mb-0 lg:col-span-2"}>
                   <h1 className={`mb-3 text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-green-500 lg:col-span-2 hover:from-green-500 hover:to-blue-500`}>
                     Build your portfolio in minutes
                   </h1>
@@ -60,6 +75,11 @@ export default function Home() {
                   </p>
                 </div>
 
+                {exampleMode &&
+                  <div className='lg:hidden relative w-full h-[447px] shadow-lg rounded-2xl'>
+                    <iframe className='rounded-2xl w-full h-[447px]' src={exampleSite} title="Example Portfolio" />
+                  </div>}
+
               </div>
 
               <div className="flex flex-col items-center w-full max-w-5xl mb-32 mt-8 lg:flex-row justify-start gap-4">
@@ -69,6 +89,12 @@ export default function Home() {
                 <a className="w-full text-center text-lg font-semibold rounded-lg px-2.5 py-2 lg:w-auto cursor-pointer" onClick={onExampleModeOn}>
                   {action2Text}
                 </a>
+              </div>
+              <div className="lg:hidden flex flex-row flex-wrap items-center justify-center gap-5 transition-all">
+                <p>Signup</p>
+                <p>Login</p>
+                <p>Stats for Hackers</p>
+                <p>Credits</p>
               </div>
             </div>
             <div>
