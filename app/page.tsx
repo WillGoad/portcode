63a7ec4182fcdaafc8d1ff4298bd6f2a2f324487
Will Goad <willgoad@btinternet.com>
2023-08-27 10:45:35 +0100

    Credits and accurate links

--- app/page.tsx
+++ app/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
   return (
     <main className="min-h-screen  p-12">
       <div className="flex flex-row justify-center gap-16">
-        <div className={!exampleMode ? "flex flex-col items-center justify-around" : "flex flex-col items-center justify-around max-w-xl"}>
+        <div className={!exampleMode ? "flex flex-col items-center justify-around gap-16" : "flex flex-col items-center justify-around max-w-xl gap-16"}>
           <header className="flex items-center sm:justify-between justify-center w-full max-w-5xl">
             <a href="https://portco.de">
               <Image
@@ -44,21 +44,17 @@ export default function Home() {
                 height={100}
               />
             </a>
-            {menuOpen && <div className="hidden lg:flex flex-row items-center justify-center gap-3 transition-all">
-              <p>Signup</p>
-              <p>•</p>
-              <p>Login</p>
-              <p>•</p>
-              <p>Stats for Hackers</p>
-              <p>•</p>
-              <p>Credits</p>
+            {menuOpen && <div className="hidden lg:flex flex-row items-center justify-center gap-5 transition-all">
+              <p><a href='https://app.portco.de/onboarding'>Signup</a></p>
+              <p><a href='https://app.portco.de/login'>Login</a></p>
+              <p><a href='https://portco.de/stats'>Stats for Hackers</a></p>
             </div>}
             {!menuOpen && <svg xmlns="http://www.w3.org/2000/svg" className="hidden lg:block" height="1em" viewBox="0 0 448 512" onClick={toggleMenu}><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" /></svg>}
             {menuOpen && <svg xmlns="http://www.w3.org/2000/svg" className="hidden lg:block" height="1em" viewBox="0 0 384 512" onClick={toggleMenu}><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" /></svg>}
           </header>
 
 
-          <div className='mt-16 flex flex-row items-start justify-center w-full max-w-5xl'>
+          <div className='flex flex-row items-start justify-center w-full max-w-5xl'>
             <div className='flex flex-col'>
               <div className="grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
 
@@ -83,7 +79,7 @@ export default function Home() {
               </div>
 
               <div className="flex flex-col items-center w-full max-w-5xl mb-32 mt-8 lg:flex-row justify-start gap-4">
-                <a href="https://app.portco.de" className="w-full mb-4 text-center text-lg font-semibold text-white bg-gradient-to-r from-blue-500 to-green-500 rounded-lg px-2.5 py-2 lg:w-auto lg:mb-0 lg:mr-4 hover:from-green-500 hover:to-blue-500 hover:border-green-500 hover:border-2 transition duration-700 hover:animate-bounce">
+                <a href="https://app.portco.de/onboarding" className="w-full mb-4 text-center text-lg font-semibold text-white bg-gradient-to-r from-blue-500 to-green-500 rounded-lg px-2.5 py-2 lg:w-auto lg:mb-0 lg:mr-4 hover:from-green-500 hover:to-blue-500 hover:border-green-500 hover:border-2 transition duration-700 hover:animate-pulse">
                   {action1Text}
                 </a>
                 <a className="w-full text-center text-lg font-semibold rounded-lg px-2.5 py-2 lg:w-auto cursor-pointer" onClick={onExampleModeOn}>
@@ -91,10 +87,9 @@ export default function Home() {
                 </a>
               </div>
               <div className="lg:hidden flex flex-row flex-wrap items-center justify-center gap-5 transition-all">
-                <p>Signup</p>
-                <p>Login</p>
-                <p>Stats for Hackers</p>
-                <p>Credits</p>
+                <p><a href='https://app.portco.de/onboarding'>Signup</a></p>
+                <p><a href='https://app.portco.de/login'>Login</a></p>
+                <p><a href='https://portco.de/stats'>Stats for Hackers</a></p>
               </div>
             </div>
             <div>
@@ -104,6 +99,8 @@ export default function Home() {
                 </div>}
             </div>
           </div>
+
+          <p className='text-center opacity-50'>Built using <a href='https://heledron.com/tools/pixel-text-generator/'>Pixel Text Generator</a>, <a href='https://fontawesome.com/'>Font Awesome</a>, <a href='https://nextjs.org/'>NextJS</a> and <a href='https://tailwindcss.com/'>TailwindCSS</a></p>
         </div>
         {exampleMode &&
           <div className='hidden lg:block relative w-[375px]  shadow-lg rounded-2xl'>
