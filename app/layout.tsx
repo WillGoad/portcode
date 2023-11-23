a47ae1fce73cd64f6b815863917cecb3741a00c8
Will Goad <will@goad.id>
2023-11-23 18:50:42 +0000

    Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Toaster } from '@/components/ui/toaster'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={inter.className}>{children}
         <Toaster />
         </body>
+        <Analytics />
     </html>
   )
 }
